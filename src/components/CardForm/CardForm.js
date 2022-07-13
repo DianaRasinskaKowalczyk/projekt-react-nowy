de13a581c0c33e12cef0ed6ac99c29c69c189ba7
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2022-07-13 13:19:34 +0200

    Add redux columnForm and cardForm

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,15 +1,19 @@
 import styles from './CardForm.module.scss';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 
 const CardForm = props => {
     const [title, setTitle] = useState('');
+
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title }, props.columnId);
+        dispatch({ type: 'ADD_CARD', payload: { title, columnId: props.columnId }});
         setTitle('');
     };
+
+    const dispatch = useDispatch();
 
 	return (
         <form className={styles.cardForm} onSubmit={handleSubmit}>
